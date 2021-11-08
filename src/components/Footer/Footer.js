29deb63c568b0f4d604929562657b0ff3579f5c1
NonADev <wesley.vieira.lima.1999@gmail.com>
2021-11-08 19:58:21 -0300

    Added text and basic style

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,36 @@
+import './Footer.scss';
+//https://www.imovelweb.com.br/imoveis-aluguel-sao-paulo-sp.html
+
 function Footer() {
     return (
         <footer>
             {/* biggest part */}
-            <div></div>
+            <div className="footer-grouplist">
+                {/* PAGES */}
+                <ul className="footer-grouplist-list">
+                    <li>Aluguel</li>
+                    <li>Compra</li>
+                    <li>Lançamentos</li>
+                    <li>Anunciar</li>
+                </ul>
+                {/* FOLLOW US */}
+                <ul className="footer-grouplist-list">
+                    <li>Instagran</li>
+                    <li>Linkedin</li>
+                </ul>
+                {/* APP LINKS */}
+                <ul className="footer-grouplist-list">
+                    <li>GOOGLE PLAY</li>
+                    <li>APPLE STORE</li>
+                </ul>
+            </div>
             {/* copyright part */}
-            <div></div>
+            <div>
+                <span>© Copyright 2021 imobiliaria.com</span>
+                <span>
+                    <a>Termos e condições de uso</a> - <a>Termos e Condições Gerais de Contratação</a> - <a>Política de privacidade</a>
+                </span>
+            </div>
         </footer>
     )
 }
